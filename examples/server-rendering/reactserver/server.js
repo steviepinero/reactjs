6c080bc0736bea54dee20f70cfb8bfb5605ffe83
Stevie <steviepinero@gmail.com>
2016-03-20 16:34:29 -0400

    react contact form

--- examples/server-rendering/reactserver/server.js
+++ examples/server-rendering/reactserver/server.js
@@ -1,21 +1,17 @@
-var React = require('react');
 var express = require('express');
-var path = require('path');
-
-// Transparently support JSX
-require('node-jsx').install();
+var stormpath = require('express-stormpath');
 
 var app = express();
 
-// All the render server does is take a CommonJS module ID and some JSON props
-// in the querystring and return a static HTML representation of the component.
-// Note that this is a backend service hit by your actual web app. Even so,
-// you would probably put Varnish in front of this in production.
-app.get('/', function(req, res){
-  var component = require(path.resolve(req.query['module']));
-  var props = JSON.parse(req.query['props'] || '{}');
-
-  res.send(React.renderComponentToString(component(props)));
+stormpath.init(app, {
+  website: true
 });
 
-app.listen(3000);
+app.on('stormpath.ready', function () {
+  app.listen(3000, 'localhost', function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('Listening at http://localhost:3000');
+  });
+});
